c6ab750599cbf0a94a0ae3e9551ca5a24fd99122
Martin <digimees@gmail.com>
2019-04-19 12:10:49 +0300

    refactor: problem 4

--- problem_4.py
+++ problem_4.py
@@ -1,14 +1,24 @@
-def solution_4():
-    largest_palindrome = 0
-    for first_num in range(100, 1000):
-        for second_num in range(100, 1000):
+def solution_4(largest_palindrome=0):
+    for first_num in range(900, 1000):
+        for second_num in range(900, 1000):
             product = first_num * second_num
-            product_string_form = str(product)
-            is_palindrome = True if product_string_form[::1] == product_string_form[::-1] else False
+            product_reversed = reverse_int(product)
+
+            is_palindrome = True if product == product_reversed else False
             is_greater_than_largest = True if product > largest_palindrome else False
+
             if (is_palindrome and is_greater_than_largest):
                 largest_palindrome = product
+
     return largest_palindrome
 
+
+def reverse_int(num):
+    new_num = 0
+    while num > 0:
+        remainder = num % 10
+        new_num = new_num * 10 + remainder
+        num = int(num / 10)
+    return new_num
 
 print(True if solution_4() == 906609 else False)
