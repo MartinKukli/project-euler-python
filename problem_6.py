859d1051a2afe7ddb1858a4703bc075dc7ad1100
Martin <digimees@gmail.com>
2019-04-20 22:44:37 +0300

    refactor: problem 6
    solution with list comprehensions

--- problem_6.py
+++ problem_6.py
@@ -1,15 +1,4 @@
-def solution_6_part_1(limit):
-    result = 0
-    for index in range(1, limit + 1):
-        result += index ** 2
-    return result
+sumOfSquares = sum([x ** 2 for x in range(1, 101)])
+squareOfSum = sum([x for x in range(1, 101)]) ** 2
 
-
-def solution_6_part_2(limit):
-    result = 0
-    for index in range(1, limit + 1):
-        result += index
-    return result ** 2
-
-
-print(True if (solution_6_part_2(100) - solution_6_part_1(100) == 25164150) else False)
+print(True if (squareOfSum - sumOfSquares == 25164150) else False)
