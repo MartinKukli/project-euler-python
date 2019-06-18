baeb7f8e246806e7551dabb9fdb5c481898318d5
Martin <digimees@gmail.com>
2019-06-18 17:51:04 +0300

    refactor: problem 25
    replaced Memoize with helper: fib_rec - uses functools lru_cache

--- python/problem_25.py
+++ python/problem_25.py
@@ -1,28 +1,10 @@
-class Memoize(object):
-    def __init__(self, func):
-        self.func = func
-        self.cache = {}
+from helpers import fib_rec
 
-    def __call__(self, *args):
-        if args in self.cache:
-            return self.cache[args]
-        ret = self.func(*args)
-        self.cache[args] = ret
-        return ret
-
-
-@Memoize
-def fib(n):
-    if n < 2:
-        return 1
-    return fib(n-2) + fib(n-1)
-
-
-def thousand_digit_fib(index=0):
+def thousand_digit_fib(index=0, length=1000):
     while True:
         index += 2
-        if len(str(fib(index))) == 1000:
+        if len(str(fib_rec(index))) == length:
             return index
 
-
-print(True if thousand_digit_fib() == 4782 else False)
+answer = 4782
+print(True if thousand_digit_fib() == answer else False)
