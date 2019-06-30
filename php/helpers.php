cfe797633bbf02fea5c690cbfac23bcd216085c8
Martin <digimees@gmail.com>
2019-06-30 12:11:54 +0300

    update: helpers
    added fib generator

--- php/helpers.php
+++ php/helpers.php
@@ -4,6 +4,20 @@ function upper_bound_for_nth_prime($n)
   return ($n < 6) ? 100 : ceil($n * (log($n) + log(log($n))));
 }
 
+function fib($limit)
+{
+  $cur = 2;
+  $prev = 1;
+
+  while ($cur < $limit) {
+    yield $cur;
+
+    $temp = $cur;
+    $cur = $prev + $cur;
+    $prev = $temp;
+  }
+}
+
 function find_primes($n)
 {
   $limit = upper_bound_for_nth_prime($n);
